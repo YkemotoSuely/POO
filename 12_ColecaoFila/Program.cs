369d4945fe3507bf91f90bec6ea758fb7e98b659
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-09-25 22:10:00 -0300

    exercício 12 - Coleção Fila

---
+++ 12_ColecaoFila/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ColecaoFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<String> filaNomes = new Queue<String>();
+            filaNomes.Enqueue("Bruno");
+            filaNomes.Enqueue("Beatriz");
+            filaNomes.Enqueue("Gustavo");
+            filaNomes.Enqueue("Caroline");
+            filaNomes.Enqueue("Cecilia");
+
+
+            Console.WriteLine("Impressão dos filaNomes");
+            Console.WriteLine($"1º) {filaNomes.Dequeue()} \n2º) {filaNomes.Dequeue()} \n3º) {filaNomes.Dequeue()}");
+
+            foreach (String nome in filaNomes)
+            {
+                Console.WriteLine($" - {nome}");
+
+            }
+        }
+    }
+}
