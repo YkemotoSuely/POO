78a11519c03883487a326338bf547b8a0af9f970
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-09-26 19:24:37 -0300

    Exercicio 14 Coleção HashSet

---
+++ 14_ HashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14__HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> setNumeros = new HashSet<int>();
+
+            setNumeros.Add(45);
+            setNumeros.Add(33);
+            setNumeros.Add(17);
+            setNumeros.Add(56);
+            setNumeros.Add(42);
+            setNumeros.Add(45);
+            setNumeros.Add(19);
+            setNumeros.Add(79);
+            setNumeros.Add(93);
+
+            Console.WriteLine("Impressão dos SetNomes");
+            foreach (int numero in setNumeros)
+            {
+                Console.WriteLine($" - {numero}");
+            }
+
+
+
+        }
+    }
+}
---
+++ 14_ HashSet/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("14_ HashSet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14_ HashSet")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d38c8893-bda8-45fa-b7ca-c82fb886eaa9")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
