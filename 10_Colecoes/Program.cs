a2fc35d9229e5c0499f895e29469e5a07b9181f6
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-09-25 22:02:09 -0300

    Exercício 11 - Coleção Lista

--- 10_Colecoes/Program.cs
+++ 10_Colecoes/Program.cs
@@ -35,6 +35,76 @@ namespace _10_Colecoes
                 //Console.WriteLine($" - ID: {aluno.id} Nome: {aluno.Nome}");
                 aluno.ImprimirAluno();
             }
+            Dictionary<int,String> dicNomes = new Dictionary<int,String>();
+            dicNomes.Add(1, "Gustavo");
+            dicNomes.Add(2, "Celso");
+            dicNomes.Add(3, "Suely");
+
+            //Dicionário não aceita chaves duplicadas
+
+            for (int i = 1; i <= dicNomes.Count; i++) 
+            {
+                Console.WriteLine($" - {dicNomes[i]}");
+
+            }
+            Dictionary<int, Aluno> dicAlunos = new Dictionary<int, Aluno>();
+            dicAlunos.Add(suely.id, suely);
+            dicAlunos.Add(celso.id, celso);
+
+            Console.WriteLine("Impressão dos alunos");
+            //for (int i = 1; i <= dicAlunos.Count; i++) 
+            //{
+            //    dicAlunos[i].ImprimirAluno();
+            //}
+
+            foreach (Aluno aluno in dicAlunos.Values) 
+            {
+                aluno.ImprimirAluno(); 
+            }
+
+
+            Queue<String> filaNomes = new Queue<String>();
+            filaNomes.Enqueue("Gustavo");
+            filaNomes.Enqueue("Celso");
+            filaNomes.Enqueue("Suely");
+
+            Console.WriteLine("Impressão dos filaNomes");
+            Console.WriteLine($"1º) {filaNomes.Dequeue()}");
+
+            foreach (String nome in filaNomes)
+            {
+                Console.WriteLine($" - {nome}");               
+
+            }
+            Stack<String> stackNomes = new Stack<String>();
+            stackNomes.Push("Gustavo");
+            stackNomes.Push("Celso");
+            stackNomes.Push("Suely");
+
+            Console.WriteLine("Impressão dos stackNomes");
+            Console.WriteLine($" 1º) {stackNomes.Pop()}");
+
+            foreach (String nome in stackNomes)
+            {
+
+                Console.WriteLine($" - {nome}");
+            }
+            HashSet<string> setNomes = new HashSet<string>();
+
+            setNomes.Add("Gustavo");
+            setNomes.Add("Celso");
+            setNomes.Add("Suely");
+            //ignora valores duplicados
+            setNomes.Add("Gustavo");
+            setNomes.Add("Celso");
+            setNomes.Add("Suely");
+
+            Console.WriteLine("Impressão dos SetNomes");
+            foreach (String nome in setNomes) 
+            { 
+                Console.WriteLine($" - {nome}"); 
+            }
+
         }
     }
     class Aluno 
---
+++ 11 _ColecaoLista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11__ColecaoLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> nomes = new List<string>();
+            nomes.Add("Bruno");
+            nomes.Add("Beatriz");
+            nomes.Add("Gustavo");
+            nomes.Add("Caroline");
+            nomes.Add("Cecilia");
+
+
+            Console.WriteLine("Impressão de nomes");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+        }
+    }
+}
