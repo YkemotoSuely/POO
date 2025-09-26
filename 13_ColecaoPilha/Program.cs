b7f34a017e12687ee615d2cd81294c43d965a23c
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-09-25 22:23:59 -0300

    Exercício 13 Coleção Pilha

---
+++ 13_ColecaoPilha/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ColecaoPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stackNumeros = new Stack<string>();
+            stackNumeros.Push("79");
+            stackNumeros.Push("80");
+            stackNumeros.Push("81");
+            stackNumeros.Push("82");
+            stackNumeros.Push("83");
+
+            Console.WriteLine("Impressão dos stackNumeros");
+            Console.WriteLine($" O último número de senha gerado foi:  {stackNumeros.Pop()}");
+
+            foreach (String numero in stackNumeros)
+            {
+
+                Console.WriteLine($" - {numero}");
+            }
+        }
+    }
+}
