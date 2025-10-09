9b5e9cda7a1bf717f7198ecb3c07b4ee3bf7bfee
Suely Ykemoto <suelyykemoto65@gmail.com>
2025-10-09 20:38:34 -0300

    Exercício 23 - leitura de Arquivo com Exceção

--- 23_LeituraArquivoComExcecao/Program.cs
+++ 23_LeituraArquivoComExcecao/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace _23_LeituraArquivoComExcecao
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite o caminho completo do arquivo que está buscando (diretório/pasta/nome do arquivo): ");
+            string caminho = Console.ReadLine();
+
+
+            try
+            {
+                string conteudo = File.ReadAllText(caminho);
+                Console.WriteLine("\nConteúdo do arquivo:");
+                Console.WriteLine(conteudo);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("\nArquivo não encontrado! Verifique o caminho e tente novamente.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine($"\n O acesso ao caminho {caminho} foi negado");
+
+            }
         }
     }
 }
